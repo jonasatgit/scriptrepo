981753419379340c94a600ce4e1eb907f589d7f0
Jonas <johms@microsoft.com>
2021-02-19 20:54:51 +0100

    minor update

--- Collections/Get-ConfigMgrCollectionEvalTimes.ps1
+++ Collections/Get-ConfigMgrCollectionEvalTimes.ps1
@@ -1,3 +1,19 @@
+#************************************************************************************************************
+# Disclaimer
+#
+# This sample script is not supported under any Microsoft standard support program or service. This sample
+# script is provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties
+# including, without limitation, any implied warranties of merchantability or of fitness for a particular
+# purpose. The entire risk arising out of the use or performance of this sample script and documentation
+# remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation,
+# production, or delivery of this script be liable for any damages whatsoever (including, without limitation,
+# damages for loss of business profits, business interruption, loss of business information, or other
+# pecuniary loss) arising out of the use of or inability to use this sample script or documentation, even
+# if Microsoft has been advised of the possibility of such damages.
+#
+#************************************************************************************************************
+# Source: https://github.com/jonasatgit/scriptrepo
+
 <#
 .Synopsis
     Script to visualize the last ConfigMgr collection evaluations in a GridView
