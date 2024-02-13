4d643a727d2c1c12f7dc3df37580af734e06585e
Jonas <jonasatgit@jonasatgit>
2024-02-14 00:30:05 +0100

    fixed search

--- General/Export-ConfigMgrWmiData.ps1
+++ General/Export-ConfigMgrWmiData.ps1
@@ -13,34 +13,24 @@
 #
 #************************************************************************************************************
 
-# To run the script as a ConfigMgr script, use the second param block to avoid issues with parameters
+# Script can be run as a ConfigMgr script
 
 # Test script to search in WMI
 
-#region param block to us the script in standalone mode
-<#
-[CmdletBinding()]
-param
-(
-    [string]$searchString = 'd740f314-c3b7-44a8-bf18-2a38b7bf7e0d',
-    [array]$WMINamespaces = ('root\ccm','ROOT\Microsoft\PolicyPlatform\Documents\Local'),
-    [bool]$OutputInfo = $true
-)
-#>
-#endregion
-
-#region param block just for the ConfigMgr scripts feature
-param
-(
-    [string]$searchString = "d740f314-c3b7-44a8-bf18-2a38b7bf7e0d"
-)
-
+#region param block without the param() string to avoid problems with the ConfigMgr scripts feature
+[string]$searchString = "f3bbbcff-67e7-402a-b952-9860e9b04cf7"
 [array]$WMINamespaces = ('root\ccm','ROOT\Microsoft\PolicyPlatform\Documents\Local')
 [bool]$OutputInfo = $true
+[bool]$CiVersionTimedOutSearch = $true
+#endregion
+
+#region excluded classes and namespaces
+[array]$excludedClassNames = @('CIM_','Synclet','_Setting','CCM_UserLogonEvents','CCM_VpnConnection','MDM_WindowsLicensing','Recently','C00000000_0000_0000_0000_000000000001')
+[array]$global:ExcludedWMINamespaces = ('root\ccm\EndpointProtection','root\ccm\RebootManagement','root\ccm\Messaging','root\ccm\Events','root\ccm\Evaltest','root\ccm\Network','root\ccm\InvAgt')
 #endregion
 
 
-
+# Get the ConfigMgr client log path from the registry   
 try
 {
     # Define the registry path for the ConfigMgr client
@@ -62,10 +52,12 @@ if (-NOT ($logPath))
 
 
 $datetimeString = get-date -Format "yyyyMMddHHmmss"
-$exportFileName = '{0}\CcmWmiExport-{1}.log' -f $logPath, $datetimeString
+# needs to be log and not txt to be able to let ConfigMgr collect the file
+$exportFileName = '{0}\_WmiExport-{1}.log' -f $logPath, $datetimeString 
 $global:dataList = [System.Collections.Generic.List[pscustomobject]]::new()
 $global:namespaceList = [System.Collections.Generic.List[string]]::new()
 $outInfo = [System.Collections.Generic.List[pscustomobject]]::new()
+$spacer = '---------------------------------------------------------------------------------------------'
 
 #region function Get-CustomWMIClasses
 function Get-CustomWMIClasses
@@ -76,7 +68,6 @@ function Get-CustomWMIClasses
     )
 
     $classList = Get-WmiObject -Namespace $rootNamespace -List
-
     foreach ($class in $classList)
     {
         if (($class.Name -imatch '^__') -or (($class.Name -imatch '^MSFT')))
@@ -102,7 +93,7 @@ function Get-WMINameSpaces
     (
         $NameSpace
     )
- 
+
     $namespaces = Get-WmiObject -Namespace $NameSpace -Class __Namespace -ErrorAction SilentlyContinue | Select-Object -Property Name
     if ($namespaces)
     {
@@ -112,40 +103,16 @@ function Get-WMINameSpaces
             $newString = '{0}\{1}' -f $NameSpace, $item.Name
 
             # lets skip some namespaces
-            if ($newString -imatch [regex]::Escape('root\ccm\EndpointProtection'))
+            $needToSkip = $false
+            foreach ($excluded in $global:ExcludedWMINamespaces)
             {
-                continue
+                if ($newString -imatch [regex]::Escape($excluded))
+                {
+                    if($OutputInfo){Write-host "Skipping namespace: `"$($newString)`"" -ForegroundColor Yellow}
+                    $needToSkip = $true
+                }
             }
-
-            if ($newString -imatch [regex]::Escape('root\ccm\RebootManagement'))
-            {
-                continue
-            }
-
-            if ($newString -imatch [regex]::Escape('root\ccm\Messaging'))
-            {
-                continue
-            }
-
-            if ($newString -imatch [regex]::Escape('root\ccm\Events'))
-            {
-                continue
-            }
-
-            if ($newString -imatch [regex]::Escape('root\ccm\Evaltest'))
-            {
-                continue
-            }
-
-            if ($newString -imatch [regex]::Escape('root\ccm\Network'))
-            {
-                continue
-            }
-
-            if ($newString -imatch [regex]::Escape('root\ccm\InvAgt'))
-            {
-                continue
-            }
+            if ($needToSkip){continue}
         
             $global:namespaceList.Add($newString)
             if($OutputInfo){Write-Host "Namespace found: $newString"}
@@ -155,14 +122,36 @@ function Get-WMINameSpaces
 
     }
 
-    if ($global:namespaceList.Contains($NameSpace))
-    {
-        # skip
-    }
-    else
+    if (-NOT ($global:namespaceList.Contains($NameSpace)))
     {
         $global:namespaceList.Add($NameSpace)
         if($OutputInfo){Write-Host "Namespace found: $NameSpace"}    
+    }
+}
+#endregion
+
+#region search for ci version timed out
+if ($CiVersionTimedOutSearch)
+{
+    if($OutputInfo){Write-Host "Parse CiaAgent.log files for VersionInfoTimedOut error.." -ForegroundColor Cyan}
+    # Example: 
+    #CIAgentJob({2535BA43-2097-45F0-A088-8D46ECE9DC5E}): CAgentJob::VersionInfoTimedOut for ModelName ScopeId_F39845A1-F303-4D3A-A303-6ECC327447D1/Application_7d1b5b09-123d-46d8-b4db-9217ce42de4f, version 12 not available.
+    [array]$SelectStringResult = Get-ChildItem -Path $logPath -Filter "CIAgent*.log" | Sort-Object -Property LastWriteTime -Descending | Select-string -Pattern "CAgentJob::VersionInfoTimedOut"
+    if($SelectStringResult)
+    {
+        $Matches.Clear()
+        $null = $SelectStringResult[0].Line -match "VersionInfoTimedOut for ModelName (?<ModelName>.*?), version (?<Version>\d+)"
+
+        # will overwrite searchString variable
+        if($OutputInfo){Write-Host "Found VersionInfoTimedOut error for CI $($Matches['ModelName'])" -ForegroundColor Cyan}
+        $searchString = $Matches['ModelName'] -replace "ScopeId_.*?/.*?_",""
+        if($OutputInfo){Write-Host "Extracted searchstring is: $($searchString)" -ForegroundColor Cyan}
+        #$Matches['Version'] # not used at the moment
+    }
+    else
+    {
+        if($OutputInfo){Write-Host "No VersionInfoTimedOut error found" -ForegroundColor Yellow}
+        if($OutputInfo){Write-Host "Will use existing searchstring: $($searchString)" -ForegroundColor Yellow}
     }
 }
 #endregion
@@ -172,7 +161,6 @@ if($OutputInfo){Write-Host "Getting list of namespaces:" -ForegroundColor Cyan}
 foreach($item in $WMINamespaces)
 {
     Get-WMINameSpaces -NameSpace $item
-    #Get-CustomWMIClasses -rootNamespace $item
 }
 
 # With all namespaces we can get a list of all classes per namespace
@@ -187,86 +175,72 @@ foreach($namespace in $global:namespaceList)
 if($OutputInfo){Write-Host "Search for string: `"$($searchString)`" in WMI classes" -ForegroundColor Cyan}
 foreach ($WMIClass in $global:dataList)
 {
+    $needToSkip = $false
     # lets skip some classes
-    if ($WMIClass.ClassName -imatch '^CIM')
+    foreach ($excluded in $excludedClassNames)
     {
-        continue
+        if ($WMIClass.ClassName -imatch [regex]::Escape($excluded))
+        {
+            if($OutputInfo){Write-host "Skipping class: `"$($WMIClass.ClassName)`"" -ForegroundColor Yellow}
+            $needToSkip = $true
+        }
     }
-
-    if ($WMIClass.ClassName -imatch 'Synclet')
-    {
-        continue
-    }
-
-    if ($WMIClass.ClassName -imatch [regex]::Escape('_Setting'))
-    {
-        continue
-    }
-
-    if ($WMIClass.ClassName -imatch [regex]::Escape('CCM_UserLogonEvents'))
-    {
-        continue
-    }
-
-    if ($WMIClass.ClassName -imatch [regex]::Escape('CCM_VpnConnection'))
-    {
-        continue
-    }    
-
-    
-    if ($WMIClass.ClassName -imatch [regex]::Escape('MDM_WindowsLicensing'))
-    {
-        continue
-    } 
-
-    if ($WMIClass.ClassName -imatch [regex]::Escape('Recently'))
-    {
-        continue
-    } 
+    if ($needToSkip){continue}
 
     $outString = '{0} - {1}' -f $WMIClass.namespace, $WMIClass.ClassName
     if($OutputInfo){Write-host $outString}
 
     try
     {
-        Get-WmiObject -Namespace ($WMIClass.Namespace) -Class ($WMIClass.ClassName) -ErrorAction Stop | ForEach-Object {
+        [array]$wmiResult = Get-CimInstance -Namespace ($WMIClass.Namespace) -Class ($WMIClass.ClassName) -ErrorAction Stop 
             
-            # export data if string was found in data
-            if ($_ | select-string -pattern $searchString)
-            {
-                if($OutputInfo){Write-Host "Data found in: `"$($WMIClass.Name)`"" -ForegroundColor Cyan}
-                $outInfo.Add($WMIClass)
-                $_ | Out-File $exportFileName -Append
-                '---------------------------------------------------------------------------------------------' | Out-File $exportFileName -Append
-            }
-            
-        }
-    }catch{}
-
-    try
-    {
-        if ($WMIClass.ClassName -imatch $searchString)
+        # export data if string was found in data
+        foreach ($item in $wmiResult)
         {
-            if($OutputInfo){Write-Host "Data found in: `"$($WMIClass.Name)`"" -ForegroundColor Cyan}
-            $outInfo.Add($WMIClass)
-            Get-WmiObject -Namespace ($WMIClass.Namespace) -Class ($WMIClass.ClassName) -ErrorAction Stop | Out-File $exportFileName -Append
-            '---------------------------------------------------------------------------------------------' | Out-File $exportFileName -Append
-        }
-    }catch{}
+            # we might need to get lazy properties
+            $itemLoaded = $null
+            $itemLoaded = $item | Get-CimInstance -ErrorAction SilentlyContinue
+            # json makes the string search easier and gives a completely expanded object
+            if (-NOT ($itemLoaded)){$itemLoaded = $item}
+            $wmiJsonString = $itemLoaded | ConvertTo-Json -Depth 100
+            if ($wmiJsonString -imatch $searchString)
+            {
+                if($OutputInfo){Write-Host "String: `"$($searchString)`" found in: `"$($WMIClass.ClassName)`"" -ForegroundColor Cyan}
+                $outInfo.Add($WMIClass)
+                $wmiJsonString | Out-File $exportFileName -Append
+                $spacer | Out-File $exportFileName -Append
+            }
+        }           
+    }
+    catch
+    {
+        $errorString = 'Error in class: {0}\{1} -> {2}' -f $WMIClass.Namespace, $WMIClass.ClassName, $_
+        if($OutputInfo){Write-Host $errorString -ForegroundColor Yellow}
+    }
 
+    # The class itself could contain the string
+    if ($WMIClass.ClassName -imatch $searchString)
+    {
+        $wmiJsonString = $WMIClass | ConvertTo-Json -Depth 100
+        if($OutputInfo){Write-Host "String: `"$($searchString)`" found in: `"$($WMIClass.ClassName)`"" -ForegroundColor Cyan}
+        $outInfo.Add($WMIClass)
+        $wmiJsonString | Out-File $exportFileName -Append
+        $spacer | Out-File $exportFileName -Append
+    }
 }
 
 
 if (Test-Path $exportFileName)
 {
+    # read all data to be able to put the names of the classes at the top of the file
     $fileContent = Get-Content $exportFileName
-
-    $outInfo | Out-File $exportFileName -Force
-    '---------------------------------------------------------------------------------------------' | Out-File $exportFileName -Append
+    $outInfo | Out-File $exportFileName -Force # clear the file via force parameter
+    $spacer | Out-File $exportFileName -Append
     $fileContent | Out-File $exportFileName -Append
 
     if($OutputInfo)
     {
+        Write-Host "Logfile: $($exportFileName)" -ForegroundColor Cyan
         Write-Host "Found the string: `"$($searchString)`" in the following classes:" -ForegroundColor Cyan
         $outInfo
     }
